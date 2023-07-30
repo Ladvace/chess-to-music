27523501b4dba265692805afd0fff9fe2310aeee
Gianmarco <cavallogianmarco@gmail.com>
2023-07-30 09:25:44 +0200

    sound improvements

--- src/ChessMusicGenerator.tsx
+++ src/ChessMusicGenerator.tsx
@@ -15,6 +15,36 @@ const piano = new Tone.Sampler({
 
 let timeouts: number[] = [];
 
+// Function to determine duration of note based on the piece moved
+const piece2duration = (move: string): string => {
+  if (move.startsWith('N')) {
+    return '4n';
+  } else if (move.startsWith('B')) {
+    return '8n';
+  } else if (move.startsWith('R')) {
+    return '2n';
+  } else if (move.startsWith('Q')) {
+    return '1m';
+  } else {
+    return '16n';
+  }
+};
+
+// Function to determine volume of note based on the piece moved
+const piece2volume = (move: string): number => {
+  if (move.startsWith('N')) {
+    return 0.5;
+  } else if (move.startsWith('B')) {
+    return 0.6;
+  } else if (move.startsWith('R')) {
+    return 0.7;
+  } else if (move.startsWith('Q')) {
+    return 0.8;
+  } else {
+    return 0.4;
+  }
+};
+
 const move2note = (move: string): string => {
   const square = move.length === 3 ? move.substring(1) : move;
   const rank = parseInt(square.substring(1));
@@ -34,10 +64,12 @@ const generateChessMusic = async (pgn: string): Promise<void> => {
 
   parsed[0].moves.forEach((move, index) => {
     const note = move2note(move.move);
+    const duration = piece2duration(move.move);
+    const volume = piece2volume(move.move);
     if (note) {
       // Ensure that note is not null before triggering sound
       timeouts.push(
-        setTimeout(() => piano.triggerAttackRelease(note, '8n'), index * 500)
+        setTimeout(() => piano.triggerAttackRelease(note, duration, Tone.now(), volume), index * 500)
       );
     }
   });
