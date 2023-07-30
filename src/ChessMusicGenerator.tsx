d7cd21a43968134a40397e5d1f3e5be9e6eaddf4
Gianmarco <cavallogianmarco@gmail.com>
2023-07-30 12:30:09 +0200

    some fixes and background effect test

--- src/ChessMusicGenerator.tsx
+++ src/ChessMusicGenerator.tsx
@@ -13,11 +13,51 @@ const piano = new Tone.Sampler({
   baseUrl: "https://tonejs.github.io/audio/salamander/",
 }).toDestination();
 
+// Create a reverb effect
+const reverb = new Tone.Reverb({
+  decay: 1.5,
+  wet: 0.4,
+}).toDestination();
+
+piano.connect(reverb);
+
+// Add a delay effect
+const delay = new Tone.FeedbackDelay('8n', 0.5).toDestination();
+piano.connect(delay);
+
+// Use a Tone.Sequence for more dynamic beat
+const synth = new Tone.Synth().toDestination();
+const notes = ['C2', 'D2', 'E2', 'A2'];
+const sequence = new Tone.Sequence((time, note) => {
+  synth.triggerAttackRelease(note, '8n', time);
+}, notes, '4n').start(0);
+
+// Vary the reverb effect over time
+Tone.Transport.scheduleRepeat(time => {
+  reverb.wet.value = Math.random() * 0.5;
+}, '4n');
+
+// Start the transport
+Tone.Transport.start();
+
 let timeouts: number[] = [];
 let recorder: Tone.Recorder | null = null;
 let recordingUrl: string | null = null;
 
-// Function to determine volume of note based on the piece moved
+const piece2duration = (move: string): string => {
+  if (move.startsWith("N")) {
+    return "4n";
+  } else if (move.startsWith("B")) {
+    return "8n";
+  } else if (move.startsWith("R")) {
+    return "2n";
+  } else if (move.startsWith("Q")) {
+    return "1m";
+  } else {
+    return "16n";
+  }
+};
+
 const piece2volume = (move: string): number => {
   if (move.startsWith("N")) {
     return 0.5;
@@ -32,28 +72,12 @@ const piece2volume = (move: string): number => {
   }
 };
 
-const piece2octave = (move: string): number => {
-  if (move.startsWith("N")) {
-    return 5;
-  } else if (move.startsWith("B")) {
-    return 4;
-  } else if (move.startsWith("R")) {
-    return 6;
-  } else if (move.startsWith("Q")) {
-    return 3;
-  } else {
-    return 4;
-  }
-};
-
 const move2note = (move: string): string => {
-  const square = move.length === 3 ? move.substring(1) : move.substring(2, 4);
-  const file = square.charAt(0);
-  const rank = parseInt(square.charAt(1));
+  const square = move.length === 3 ? move.substring(1) : move.substring(0, 2);
+  const rank = parseInt(square.substring(1));
   const notes = ["C", "D", "E", "F", "G", "A", "B"];
-  const noteIndex = (file.charCodeAt(0) - "a".charCodeAt(0) + rank - 1) % notes.length;
-  const note = notes[noteIndex];
-  const octave = piece2octave(move);
+  const octave = Math.floor((rank - 1) / notes.length) + 4;
+  const note = notes[(rank - 1) % notes.length];
   return note + octave;
 };
 
@@ -67,12 +91,12 @@ const generateChessMusic = async (pgn: string): Promise<void> => {
 
   parsed[0].moves.forEach((move, index) => {
     const note = move2note(move.move);
+    const duration = piece2duration(move.move);
     const volume = piece2volume(move.move);
     if (note) {
-      // Ensure that note is not null before triggering sound
       timeouts.push(
         setTimeout(
-          () => piano.triggerAttackRelease(note, '8n', Tone.now(), volume),
+          () => piano.triggerAttackRelease(note, duration, Tone.now(), volume),
           index * 500
         )
       );
@@ -99,6 +123,7 @@ const ChessMusicGenerator = () => {
     // Clear all timeouts and stop the music
     timeouts.forEach((timeout) => clearTimeout(timeout));
     piano.releaseAll();
+    sequence.stop();
 
     // Stop the recording and generate a Blob
     if (recorder) {
@@ -116,18 +141,6 @@ const ChessMusicGenerator = () => {
       link.download = "chess-music.mp3";
       link.click();
     }
-  };
-
-  const onResume = async () => {
-    if (!isPlaying()) {
-      setIsPlaying(true);
-      await generateChessMusic(pgn());
-    }
-  };
-
-  const onRestart = async () => {
-    onStop();
-    onStart();
   };
 
   return (
@@ -152,19 +165,6 @@ const ChessMusicGenerator = () => {
           class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
         >
           Stop Music
-        </button>
-        <button
-          onClick={onResume}
-          disabled={isPlaying()}
-          class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          Resume Music
-        </button>
-        <button
-          onClick={onRestart}
-          class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Restart Music
         </button>
         <button
           onClick={onDownload}
