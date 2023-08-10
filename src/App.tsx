b22f348350e1ecfd9ad2f065f55f4d2fa6db245a
Gianmarco <cavallogianmarco@gmail.com>
2023-08-10 14:24:35 +0200

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import type { Component } from "solid-js";
 import ChessMusicGenerator from "./ChessMusicGenerator";
+import { inject } from "@vercel/analytics";
 
 const App: Component = () => {
+  inject();
+
   return (
     <div class="h-screen">
       <ChessMusicGenerator />
